467d968e58c30979491e710555c52c0b2a17e65f
Andrew Barnert <abarnert@yahoo.com>
2013-04-10 15:46:51 -0700

    2.x port
    - A 2to3- or 3to2-based automated port seems unlikely, so I split
      the code into separate directories, started with a 3to2 fix,
      and hacked on it until it worked with both 2.5 and 2.7. The
      tests are based on the 2.7 stdlib tests, not the 3.4 versions
      (because way too much other stuff changed in 3.x--in particular,
      the tests depend on what opcodes exist, how methods in classes
      work, etc., in ways that the actual code doesn't), so they may
      not be all that good.
    
    - Also added a workaround for the setuptools bug described at
      http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html.

--- docs/versions.rst
+++ docs/versions.rst
@@ -2,6 +2,13 @@
 Version History
 ==========
 
+0.2
+    * 2.x backport (starting with 3to2, but lots of manual work
+      required, and test script is adapted from stdlib dis instead of
+      3.4 dis).
+    * Workaround for silly setuptools bug in test (see
+      http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html).
+
 0.1
     * First implementation, built from dis_api3.diff patch against 
       rev 308b216f084f.
--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
+# Hack to prevent stupid error on exit of `python setup.py test`. (See
+# http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html.)
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
 import sys
 
 from setuptools import setup
 
 extra_setup = {}
-if sys.version_info < (3,):
-    # TODO if possible
-    sys.stderr.write('This backport is for Python 3.x only.\n')
-    sys.exit(1)
+if sys.version_info >= (3, 0):
+    root_dir = 'dis34_3x'
 else:
-    # extra_setup['use_2to3'] = True
-    pass
+    root_dir = 'dis34_2x'
 
 setup(
     name='dis34',
-    version='0.1',
-    description='Backport of new dis module from 3.4 to earlier 3.x.',
+    version='0.2',
+    description='Backport of new dis module from 3.4.',
     long_description=(open('README.rst').read() + '\n\n' +
                       open('docs/dis34.rst').read() + '\n\n' +
                       open('docs/versions.rst').read()),
     py_modules=['dis34'],
+    package_dir={'': root_dir},
     maintainer='Andrew Barnert',
     maintainer_email='abarnert@yahoo.com',
     license='PSF license',
@@ -28,6 +33,10 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: Python Software Foundation License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3'],
